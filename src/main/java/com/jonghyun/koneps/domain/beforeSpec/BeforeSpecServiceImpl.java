4286e557cf00248692fe70be1a4d96c44f9fceae
jonghyun <starrything@gmail.com>
2022-04-07 16:37:49 +0900

    # 전체 조회 메소드 분리 - 사전규격 - 입찰공고

--- src/main/java/com/jonghyun/koneps/domain/beforeSpec/BeforeSpecServiceImpl.java
+++ src/main/java/com/jonghyun/koneps/domain/beforeSpec/BeforeSpecServiceImpl.java
@@ -17,7 +17,38 @@ public class BeforeSpecServiceImpl implements BeforeSpecService {
     @Override
     public List<Map<String, Object>> searchBeforeSpecList(String keyword) {
         List<Map<String, Object>> result = new ArrayList<>();
-        beforeSpecificationRepository.findByBfSpecRgstNoContainsOrPrdctClsfcNoNmContainsOrRlDminsttNmContainsOrderByRcptDtDesc(keyword, keyword, keyword)
+
+        if (keyword.length() > 0) {
+            List<Map<String, Object>> finalResult = result;
+            beforeSpecificationRepository.findByBfSpecRgstNoContainsOrPrdctClsfcNoNmContainsOrRlDminsttNmContainsOrderByRcptDtDesc(keyword, keyword, keyword)
+                    .stream()
+                    .forEach(beforeSpec -> {
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("rcptDt", beforeSpec.getRcptDt());
+                        map.put("bfSpecRgstNo", beforeSpec.getBfSpecRgstNo());
+                        map.put("prdctClsfcNoNm", beforeSpec.getPrdctClsfcNoNm());
+                        map.put("asignBdgtAmt", beforeSpec.getAsignBdgtAmt());
+                        map.put("opninRgstClseDt", beforeSpec.getOpninRgstClseDt());
+                        map.put("rlDminsttNm", beforeSpec.getRlDminsttNm());
+                        map.put("dlvrTmlmtDt", beforeSpec.getDlvrTmlmtDt());
+                        map.put("asignBdgtAmt", beforeSpec.getAsignBdgtAmt());
+                        map.put("dlvrDaynum", beforeSpec.getDlvrDaynum());
+                        map.put("specDocFileUrl1", beforeSpec.getSpecDocFileUrl1());
+                        map.put("specDocFileUrl2", beforeSpec.getSpecDocFileUrl2());
+
+                        finalResult.add(map);
+                    });
+            result = finalResult;
+        } else {
+            result = this.getBeforeSpecListAll();
+        }
+
+        return result;
+    }
+
+    private List<Map<String, Object>> getBeforeSpecListAll() {
+        List<Map<String, Object>> result = new ArrayList<>();
+        beforeSpecificationRepository.findAll()
                 .stream()
                 .forEach(beforeSpec -> {
                     Map<String, Object> map = new HashMap<>();
@@ -35,7 +66,6 @@ public class BeforeSpecServiceImpl implements BeforeSpecService {
 
                     result.add(map);
                 });
-
         return result;
     }
 }
--- src/main/java/com/jonghyun/koneps/domain/bidNotice/BidNoticeServiceImpl.java
+++ src/main/java/com/jonghyun/koneps/domain/bidNotice/BidNoticeServiceImpl.java
@@ -17,7 +17,49 @@ public class BidNoticeServiceImpl implements BidNoticeService {
     @Override
     public List<Map<String, Object>> searchBidNoticeList(String keyword) {
         List<Map<String, Object>> result = new ArrayList<>();
-        bidNoticeRepository.findByBidNtceNoContainsOrBidNtceNmContainsOrNtceInsttNmContainsOrDminsttNmContainsOrderByBidNtceDtDesc(keyword, keyword, keyword, keyword)
+        if (keyword.length() > 0) {
+            List<Map<String, Object>> finalResult = result;
+            bidNoticeRepository.findByBidNtceNoContainsOrBidNtceNmContainsOrNtceInsttNmContainsOrDminsttNmContainsOrderByBidNtceDtDesc(keyword, keyword, keyword, keyword)
+                    .stream()
+                    .forEach(bidNotice -> {
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("bidNtceDt", bidNotice.getBidNtceDt());
+                        map.put("bidNtceNo", bidNotice.getBidNtceNo());
+                        map.put("bidNtceNm", bidNotice.getBidNtceNm());
+                        map.put("ntceInsttCd", bidNotice.getNtceInsttCd());
+                        map.put("ntceInsttNM", bidNotice.getNtceInsttNm());
+                        map.put("dminsttCd", bidNotice.getDminsttCd());
+                        map.put("dminsttNm", bidNotice.getDminsttNm());
+                        map.put("asignBdgtAmt", bidNotice.getAsignBdgtAmt());
+                        map.put("presmptPrce", bidNotice.getPresmptPrce());
+                        map.put("bidMethdNm", bidNotice.getBidMethdNm());
+                        map.put("cntrctCnclsMthdNm", bidNotice.getCntrctCnclsMthdNm());
+                        map.put("bidClseDt", bidNotice.getBidClseDt());
+                        map.put("opengDt", bidNotice.getOpengDt());
+                        map.put("sucsfbidMthdNm", bidNotice.getSucsfbidMthdNm());
+                        map.put("ntceSpecDocUrl1", bidNotice.getNtceSpecDocUrl1());
+                        map.put("ntceSpecDocUrl2", bidNotice.getNtceSpecDocUrl2());
+                        map.put("ntceSpecDocUrl3", bidNotice.getNtceSpecDocUrl3());
+                        map.put("ntceSpecFileNm1", bidNotice.getNtceSpecFileNm1());
+                        map.put("ntceSpecFileNm2", bidNotice.getNtceSpecFileNm2());
+                        map.put("ntceSpecFileNm3", bidNotice.getNtceSpecFileNm3());
+                        map.put("bidNtceDtlUrl", bidNotice.getBidNtceDtlUrl());
+                        map.put("bidNtceUrl", bidNotice.getBidNtceUrl());
+                        map.put("rgstDt", bidNotice.getRgstDt());
+
+                        finalResult.add(map);
+                    });
+            result = finalResult;
+        } else {
+            result = this.getBidNoticeListAll();
+        }
+
+        return result;
+    }
+
+    private List<Map<String, Object>> getBidNoticeListAll() {
+        List<Map<String, Object>> result = new ArrayList<>();
+        bidNoticeRepository.findAll()
                 .stream()
                 .forEach(bidNotice -> {
                     Map<String, Object> map = new HashMap<>();
@@ -47,7 +89,6 @@ public class BidNoticeServiceImpl implements BidNoticeService {
 
                     result.add(map);
                 });
-
         return result;
     }
 }
